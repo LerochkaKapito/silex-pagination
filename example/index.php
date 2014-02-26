e0b89b5660b6808c51792e641a0026cf376f1064
Kilte <nwotnbm@gmail.com>
2014-02-26 15:51:59 +0400

    Update example.

--- example/index.php
+++ example/index.php
@@ -3,35 +3,40 @@
 /**
  * Part of the SilexPagination
  *
- * @author Kilte <nwotnbm@gmail.com>
+ * @author  Kilte <nwotnbm@gmail.com>
  * @package SilexPagination
  */
 
 use Kilte\Silex\Pagination\PaginationServiceProvider;
-use Kilte\View\ViewServiceProvider;
 use Silex\Application;
 use Silex\Provider\TwigServiceProvider;
 
 require __DIR__ . '/../vendor/autoload.php';
-
+error_reporting(-1);
+ini_set('display_errors', 1);
 $app = new Application(array('debug' => true));
 
 $app->register(new PaginationServiceProvider, array('pagination.per_page' => 5))
-    ->register(new ViewServiceProvider, array('view.path' => __DIR__ . '/views/'))
-    ->register(new TwigServiceProvider, array('twig.path' =>__DIR__ . '/views/'))
-;
-$app->get(
-    '/',
-    function () use ($app) {
-        /** @var \Kilte\Silex\Pagination\PaginationService $pagination */
-        $pagination = $app['pagination'](100, 10);
-        $pages = $pagination->build();
-        $rawPHPView = $app['view']('pagination', array('pages' => $pages));
-        /** @var $twig \Twig_Environment */
-        $twig = $app['twig'];
-        $twigView = $twig->render('pagination.twig', array('pages' => $pages, 'current' => $pagination->currentPage()));
-        return $rawPHPView . $twigView;
-    }
-);
+    ->register(new TwigServiceProvider, array('twig.path' => __DIR__ . '/views/'));
+$app
+    ->get(
+        '/{page}',
+        function ($page) use ($app) {
+            /** @var \Kilte\Pagination\Pagination $pagination */
+            $pagination = $app['pagination'](100, $page);
+            $pages      = $pagination->build();
+            /** @var $twig \Twig_Environment */
+            $twig = $app['twig'];
+
+            return $twig->render('pagination.twig', array('pages' => $pages, 'current' => $pagination->currentPage()));
+        }
+    )
+    ->value('page', 1)
+    ->convert(
+        'page',
+        function ($page) {
+            return (int) $page;
+        }
+    );
 
 $app->run();
