3b42c51223fa2599ed5716de2f184edad1c6ce62
Kilte Leichnam <nwotnbm@gmail.com>
2014-07-03 21:21:44 +0400

    Added tests

---
+++ src/Kilte/Silex/Pagination/Tests/PaginationServiceProviderTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Part of the SilexPagination
+ *
+ * @author  Kilte Leichnam <nwotnbm@gmail.com>
+ * @package SilexPagination
+ */
+
+namespace Kilte\Silex\Pagination\Tests;
+
+use Kilte\Silex\Pagination\PaginationServiceProvider;
+use Silex\Application;
+
+/**
+ * Class PaginationServiceProviderTest
+ *
+ * @package Kilte\Silex\Pagination\Tests
+ */
+class PaginationServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testRegisterDefaultValues()
+    {
+        $app = new Application();
+        $app->register(new PaginationServiceProvider());
+        $this->assertEquals(20, $app['pagination.per_page']);
+        $this->assertEquals(4, $app['pagination.neighbours']);
+        /** @var $pagination \Kilte\Pagination\Pagination */
+        $pagination = $app['pagination'](100, 5);
+        $this->assertInstanceOf('\\Kilte\\Pagination\\Pagination', $pagination);
+    }
+
+    public function testRegisterCustomValues()
+    {
+        $app = new Application();
+        $app->register(
+            new PaginationServiceProvider(),
+            array('pagination.per_page' => 2, 'pagination.neighbours' => 2)
+        );
+        $this->assertEquals(2, $app['pagination.per_page']);
+        $this->assertEquals(2, $app['pagination.neighbours']);
+        /** @var $pagination \Kilte\Pagination\Pagination */
+        $pagination = $app['pagination'](100, 5);
+        $this->assertInstanceOf('\\Kilte\\Pagination\\Pagination', $pagination);
+    }
+
+}
